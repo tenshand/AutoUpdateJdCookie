9efe24a57d71c31ca147f76578000b455b829043
tenshand <125197881+tenshand@users.noreply.github.com>
2025-03-31 20:31:26 +0800

    Update config_example.py

--- config_example.py
+++ config_example.py
@@ -6,7 +6,7 @@ user_datas = {
         "sms_func": "manual_input",
         # "sms_webhook": "https://127.0.0.1:3000/getCode",
         # 设置为True时, 即使账号未失效也更新
-        "force_update": true
+        "force_update": True
     }
 }
 
